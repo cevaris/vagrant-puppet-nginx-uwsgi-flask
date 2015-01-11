aca146ebe0a114828e1473ed2050d91afe486532
Adam Cardenas <cevaris@gmail.com>
2015-01-11 15:18:43 -0700

    modified site to invoke third party nginx/python puppet modules

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,13 +1,25 @@
 node default {
     exec { 'apt-update':
-      command => '/usr/bin/apt-get update',
+        command => '/usr/bin/apt-get update',
     }
 
     Exec["apt-update"] -> Package <| |>
 
     include users
-    include nginx
-    include python
+    class { 'nginx':
+        package_source  => 'passenger',
+        http_cfg_append => {
+            'passenger_root' => '/usr/lib/ruby/vendor_ruby/phusion_passenger/locations.ini',
+        }
+    }
+    include nginx_custom
+    class { 'python' :
+        version    => 'system',
+        pip        => true,
+        virtualenv => true,
+        # gunicorn   => true,
+    }
+    include python_custom
     include uwsgi
-    include utils
+    include base
 }
