d1cb012f8af838a0390d7a0ccf0fdad89c1ad6d7
Adam Cardenas <cevaris@gmail.com>
2015-01-11 15:21:43 -0700

    updated with correct virtualenv config

--- puppet/modules/python_custom/manifests/init.pp
+++ puppet/modules/python_custom/manifests/init.pp
@@ -1,41 +1,12 @@
 class python_custom {
 
+    python::virtualenv { '/var/www/myapp.example.co.uk' :
+        ensure       => present,
+        requirements => '/var/www/myapp.example.co.uk/src/requirements.txt',
+        venv_dir     => '/var/www/myapp.example.co.uk/venv',
+        owner        => 'root',
+        group        => 'www-data',
+        mode         => '0755',
+    }
 
-  python::virtualenv { '/var/www/myapp.example.co.uk' :
-    ensure       => present,
-    version      => 'system',
-    requirements => '/var/www/myapp.example.co.uk/requirements.txt',
-    proxy        => 'http://proxy.domain.com:3128',
-    systempkgs   => true,
-    distribute   => false,
-    venv_dir     => '/home/www-data/virtualenvs',
-    owner        => 'www-data',
-    group        => 'www-data',
-    cwd          => '/var/www/myapp.example.co.uk',
-    timeout      => 0,
-  }
-  
-    # include python::packages
-
-  
-
-    # package { 'python':
-    #     ensure => installed,
-    # }
 }
-
-# class python::packages {
-#     $apt = ['python-dev', 'build-essential', 'python-pip', ]
-#     $pip = ['flask', ]
-
-#     package { $apt:
-#         require => Class['python'],
-#         ensure => installed,
-#     }
-
-#     package { $pip:
-#         require => Class['python'],
-#         ensure => installed,
-#         provider => pip,
-#     }
-# }
